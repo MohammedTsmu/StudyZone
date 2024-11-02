85d0073a66b8017a15803a4aaa059543bdc961c7
Dr. Mohammed Qasim <87497268+MohammedTsmu@users.noreply.github.com>
2024-11-02 07:37:00 +0300

    Clean Up Code + Update for new version v1.0.1

---
+++ StudyZone/SettingsForm.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace StudyZone
+{
+    partial class SettingsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxPauseReminders = new System.Windows.Forms.GroupBox();
+            this.chkEnablePauseReminders = new System.Windows.Forms.CheckBox();
+            this.lblReminderThreshold = new System.Windows.Forms.Label();
+            this.nudReminderThreshold = new System.Windows.Forms.NumericUpDown();
+            this.lblReminderInterval = new System.Windows.Forms.Label();
+            this.nudReminderInterval = new System.Windows.Forms.NumericUpDown();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.groupBoxPauseReminders.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudReminderThreshold)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudReminderInterval)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBoxPauseReminders
+            // 
+            this.groupBoxPauseReminders.Controls.Add(this.btnCancel);
+            this.groupBoxPauseReminders.Controls.Add(this.btnSave);
+            this.groupBoxPauseReminders.Controls.Add(this.nudReminderInterval);
+            this.groupBoxPauseReminders.Controls.Add(this.lblReminderInterval);
+            this.groupBoxPauseReminders.Controls.Add(this.nudReminderThreshold);
+            this.groupBoxPauseReminders.Controls.Add(this.lblReminderThreshold);
+            this.groupBoxPauseReminders.Controls.Add(this.chkEnablePauseReminders);
+            this.groupBoxPauseReminders.Location = new System.Drawing.Point(13, 13);
+            this.groupBoxPauseReminders.Name = "groupBoxPauseReminders";
+            this.groupBoxPauseReminders.Size = new System.Drawing.Size(775, 425);
+            this.groupBoxPauseReminders.TabIndex = 0;
+            this.groupBoxPauseReminders.TabStop = false;
+            this.groupBoxPauseReminders.Text = "Pause Reminder Settings";
+            // 
+            // chkEnablePauseReminders
+            // 
+            this.chkEnablePauseReminders.AutoSize = true;
+            this.chkEnablePauseReminders.Location = new System.Drawing.Point(7, 22);
+            this.chkEnablePauseReminders.Name = "chkEnablePauseReminders";
+            this.chkEnablePauseReminders.Size = new System.Drawing.Size(183, 20);
+            this.chkEnablePauseReminders.TabIndex = 0;
+            this.chkEnablePauseReminders.Text = "Enable Pause Reminders";
+            this.chkEnablePauseReminders.UseVisualStyleBackColor = true;
+            this.chkEnablePauseReminders.CheckedChanged += new System.EventHandler(this.chkEnablePauseReminders_CheckedChanged);
+            // 
+            // lblReminderThreshold
+            // 
+            this.lblReminderThreshold.AutoSize = true;
+            this.lblReminderThreshold.Location = new System.Drawing.Point(7, 49);
+            this.lblReminderThreshold.Name = "lblReminderThreshold";
+            this.lblReminderThreshold.Size = new System.Drawing.Size(190, 16);
+            this.lblReminderThreshold.TabIndex = 1;
+            this.lblReminderThreshold.Text = "Reminder Threshold (minutes):";
+            // 
+            // nudReminderThreshold
+            // 
+            this.nudReminderThreshold.Location = new System.Drawing.Point(7, 69);
+            this.nudReminderThreshold.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.nudReminderThreshold.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudReminderThreshold.Name = "nudReminderThreshold";
+            this.nudReminderThreshold.Size = new System.Drawing.Size(120, 22);
+            this.nudReminderThreshold.TabIndex = 2;
+            this.nudReminderThreshold.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // lblReminderInterval
+            // 
+            this.lblReminderInterval.AutoSize = true;
+            this.lblReminderInterval.Location = new System.Drawing.Point(7, 98);
+            this.lblReminderInterval.Name = "lblReminderInterval";
+            this.lblReminderInterval.Size = new System.Drawing.Size(172, 16);
+            this.lblReminderInterval.TabIndex = 3;
+            this.lblReminderInterval.Text = "Reminder Interval (minutes):";
+            // 
+            // nudReminderInterval
+            // 
+            this.nudReminderInterval.Location = new System.Drawing.Point(7, 118);
+            this.nudReminderInterval.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.nudReminderInterval.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.nudReminderInterval.Name = "nudReminderInterval";
+            this.nudReminderInterval.Size = new System.Drawing.Size(120, 22);
+            this.nudReminderInterval.TabIndex = 4;
+            this.nudReminderInterval.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(7, 147);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(7, 177);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // SettingsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBoxPauseReminders);
+            this.Name = "SettingsForm";
+            this.Text = "Settings";
+            this.groupBoxPauseReminders.ResumeLayout(false);
+            this.groupBoxPauseReminders.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudReminderThreshold)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudReminderInterval)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxPauseReminders;
+        private System.Windows.Forms.NumericUpDown nudReminderThreshold;
+        private System.Windows.Forms.Label lblReminderThreshold;
+        private System.Windows.Forms.CheckBox chkEnablePauseReminders;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.NumericUpDown nudReminderInterval;
+        private System.Windows.Forms.Label lblReminderInterval;
+    }
+}
---
+++ StudyZone/SettingsForm.cs
@@ -0,0 +1,132 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudyZone
+{
+    //public partial class SettingsForm : Form
+    //{
+    //    public SettingsForm()
+    //    {
+    //        InitializeComponent();
+    //    }
+
+    //    //private void chkEnablePauseReminders_CheckedChanged(object sender, EventArgs e)
+    //    //{
+
+    //    //}
+    //    private void chkEnablePauseReminders_CheckedChanged(object sender, EventArgs e)
+    //    {
+    //        bool enabled = chkEnablePauseReminders.Checked;
+    //        nudReminderThreshold.Enabled = enabled;
+    //        nudReminderInterval.Enabled = enabled;
+    //    }
+
+    //    //private void btnSave_Click(object sender, EventArgs e)
+    //    //{
+
+    //    //}
+    //    private void btnSave_Click(object sender, EventArgs e)
+    //    {
+    //        // Save the settings
+    //        // We'll implement this later
+    //    }
+
+    //    //private void btnCancel_Click(object sender, EventArgs e)
+    //    //{
+
+    //    //}
+    //    private void btnCancel_Click(object sender, EventArgs e)
+    //    {
+    //        this.Close();
+    //    }
+
+    //}
+    public partial class SettingsForm : Form
+    {
+        private AppSettings appSettings;
+
+        public SettingsForm()
+        {
+            InitializeComponent();
+
+            // Load settings
+            LoadSettings();
+        }
+
+        private void LoadSettings()
+        {
+            // Load settings from file
+            appSettings = LoadAppSettings();
+
+            // Set control values
+            chkEnablePauseReminders.Checked = appSettings.EnablePauseReminders;
+            nudReminderThreshold.Value = appSettings.PauseReminderThreshold;
+            nudReminderInterval.Value = appSettings.PauseReminderInterval;
+
+            // Enable or disable numeric up-down controls
+            nudReminderThreshold.Enabled = chkEnablePauseReminders.Checked;
+            nudReminderInterval.Enabled = chkEnablePauseReminders.Checked;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // Update settings
+            appSettings.EnablePauseReminders = chkEnablePauseReminders.Checked;
+            appSettings.PauseReminderThreshold = (int)nudReminderThreshold.Value;
+            appSettings.PauseReminderInterval = (int)nudReminderInterval.Value;
+
+            // Save settings to file
+            SaveAppSettings(appSettings);
+
+            // Close the form
+            this.Close();
+        }
+
+        // Methods to load and save settings
+        private AppSettings LoadAppSettings()
+        {
+            string filePath = Path.Combine(Application.LocalUserAppDataPath, "settings.json");
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                return JsonConvert.DeserializeObject<AppSettings>(json);
+            }
+            else
+            {
+                // Return default settings
+                return new AppSettings
+                {
+                    StartWithWindows = false,
+                    EnablePauseReminders = true,
+                    PauseReminderThreshold = 5,
+                    PauseReminderInterval = 2
+                };
+            }
+        }
+
+        private void SaveAppSettings(AppSettings settings)
+        {
+            string filePath = Path.Combine(Application.LocalUserAppDataPath, "settings.json");
+            Directory.CreateDirectory(Application.LocalUserAppDataPath);
+            string json = JsonConvert.SerializeObject(settings, Formatting.Indented);
+            File.WriteAllText(filePath, json);
+        }
+
+        private void chkEnablePauseReminders_CheckedChanged(object sender, EventArgs e)
+        {
+            bool enabled = chkEnablePauseReminders.Checked;
+            nudReminderThreshold.Enabled = enabled;
+            nudReminderInterval.Enabled = enabled;
+        }
+    }
+
+}
